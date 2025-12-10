a37aad8628d3f5a2afc6dde49b7ea5f47acf0d7d
duduusxz <eduardoataidemelo@gmail.com>
2025-12-10 19:47:24 -0300

    Arrumando a main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-import classes.Caneta;
-
 public class Main {
     public static void main(String[] args) {
-        Caneta caneta1 = new Caneta();
-
-        caneta1.modelo = "Bic";
-        caneta1.cor = "Verde";
-        caneta1.ponta = 0.2;
-        caneta1.tampar();
-        caneta1.status();
-        caneta1.escrever();
-        caneta1.destampar();
-        caneta1.escrever();
+        System.out.println("This is my Main.");
     }
 }
