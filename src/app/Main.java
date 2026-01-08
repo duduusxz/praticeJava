280335d80c57f3ed1cfd326c37856d5e8418eb8d
duduusxz <eduardoataidemelo@gmail.com>
2026-01-08 00:03:56 -0300

    Aprendendo polimorfismo na prática com o sobrecarga.

--- src/app/Main.java
+++ src/app/Main.java
@@ -24,8 +24,12 @@ public class Main {
         arara.add(new Arara(10F, 20, 30, "Azul"));;
         arara.get(0).emitirSom();
 
-        peixeDourado.add(new PeixeDourado(10F, 20, 30, "Azul"));;
-        peixeDourado.get(0).emitirSom();
+        peixeDourado.add(new PeixeDourado(10F, 20, 30, "Azul"));
+
+        cachorro.get(0).reagir(10, 15);
+        cachorro.get(0).reagir(true);
+        cachorro.get(0).reagir(20F, 3);
+        cachorro.get(0).reagir("Cheguei");
     }
 
 }
--- src/model/Cachorro.java
+++ src/model/Cachorro.java
@@ -12,4 +12,48 @@ public class Cachorro extends Mamifero{
     public void abanarRabo(){
         System.out.println("Abandando Rabo");
     }
+
+    public void reagir(String frase){
+        if (frase.equals("Comida dog") || frase.equals("Cheguei")){
+            System.out.println("Abanando rabo");
+        } else {
+            System.out.println("Cachorro está rosnando");
+        }
+    }
+    
+    public void reagir(int hora, int minuto){
+        if (hora < 12){
+            System.out.println("Abandando rabo");
+        } else if (hora >= 18){
+            System.out.println("Latindo");
+        } else {
+            System.out.println("Abanando rabo e latindo");
+        }
+    }
+
+    public void reagir(boolean dono){
+        if (dono){
+            System.out.println("Abandando rabo");
+        } else {
+            System.out.println("Latir");
+        }
+    }
+
+    public void reagir(float peso, int idade){
+        if (idade < 5) {
+
+            if (peso < 10){
+                System.out.println("Abandando rabo");
+            } else {
+                System.out.println("Latindo");
+            }
+        } else {
+            if (peso < 10){
+                System.out.println("Rosnando");
+            } else {
+                System.out.println("Ignorar");
+            }
+        }
+    }
+
 }
